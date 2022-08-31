2144b0b9a7b0272dc885330975039dd5013db560
Valentin Huber <contact@valentinhuber.me>
2022-08-31 16:46:06 -0400

    fix api path resolve #1

--- pages/api/[lang].ts
+++ pages/api/[lang].ts
@@ -1,14 +1,15 @@
 import { questions } from '../../public/questions'
 import fs from 'fs'
 import { apiify } from '../../helpers/apiify'
-
+import path from 'path'
 
 export default apiify(
   ( req, res ): void => {
+    const dir = path.resolve( './public',`locales/${req.query.lang}` )
     try {
       const translations = {
-        questions: JSON.parse( '' + fs.readFileSync( `./public/locales/${req.query.lang}/questions.json` ) ),
-        tags: JSON.parse( '' + fs.readFileSync( `./public/locales/${req.query.lang}/tags.json` ) )
+        questions: JSON.parse( '' + fs.readFileSync( path.join( dir, 'questions.json' ) ) ),
+        tags: JSON.parse( '' + fs.readFileSync( path.join( dir, 'tags.json' ) ) )
       }
       res.json( { questions: questions, translations } )
     } catch ( e ) {
