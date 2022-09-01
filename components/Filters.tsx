77ccd13ad53b2ba4e55e053e80c17db27cd0de9a
Valentin Huber <contact@valentinhuber.me>
2022-08-31 20:41:01 -0400

    Adding Dropdown Component #2

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,15 +1,15 @@
-import { Bars3Icon } from '@heroicons/react/20/solid'
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
 import { Question } from '../public/questions'
 import { TriStateSwitch, TriStateSwitchState } from './TriStateSwitch'
 import { questions as allQuestions } from '../public/questions'
+import { Dropdown } from './Dropdown'
 
 const possibleDeepnessLevels = Array( 5 ).fill( 0 ).map( ( _, i ) => i+1 )
-const tags = allQuestions.map( e => e.tags ).flat().filter( ( e,i,a ) => a.indexOf( e ) === i )
+const allTags = allQuestions.map( e => e.tags ).flat().filter( ( e,i,a ) => a.indexOf( e ) === i )
 const DEFAULT_TAG_STATE: TriStateSwitchState = 'IGNORE'
+// const QUERY_TAG_FILTER_PREFIX = 'tag_'
 
-type QuestionFilter =( q: Question ) => boolean
+type QuestionFilter = ( q: Question ) => boolean
 
 interface FiltersProps {
   filters: QuestionFilters
@@ -21,15 +21,15 @@ interface QuestionFilters extends QuestionFiltersData {
 }
 
 interface QuestionFiltersData {
-tagStates: Record<string, TriStateSwitchState>;
-minDeepnessState: number;
-maxDeepnessState: number;
+  tags: Record<string, TriStateSwitchState>;
+  minDeepness: number;
+  maxDeepness: number;
 }
 
 export const defaultFiltersObject: QuestionFilters = {
-  tagStates: tags.map( e => ( { [e]: DEFAULT_TAG_STATE } ) ).reduce( ( acc, cur ) => Object.assign( acc, cur ), {} ),
-  minDeepnessState: possibleDeepnessLevels[0],
-  maxDeepnessState: possibleDeepnessLevels[possibleDeepnessLevels.length-1],
+  tags: allTags.map( e => ( { [e]: DEFAULT_TAG_STATE } ) ).reduce( ( acc, cur ) => Object.assign( acc, cur ), {} ),
+  minDeepness: possibleDeepnessLevels[0],
+  maxDeepness: possibleDeepnessLevels[possibleDeepnessLevels.length-1],
   filterFunction: () => true
 }
 
@@ -39,57 +39,55 @@ export const Filters = ( { filters, setFilters }: FiltersProps ): JSX.Element =>
 
   const updateFilters = ( state: QuestionFiltersData ): void => {
     const filters: QuestionFilter[] = []
-    filters.push( ...getFiltersForTags( state.tagStates ) )
-    filters.push( q => q.deepness >= state.minDeepnessState )
-    filters.push( q => q.deepness <= state.maxDeepnessState )
+    filters.push( ...getFiltersForTags( state.tags ) )
+    filters.push( q => q.deepness >= state.minDeepness )
+    filters.push( q => q.deepness <= state.maxDeepness )
     setFilters( {
       ...state,
       filterFunction: reduceFilters( filters )
     } )
   }
 
-  const [hidden, setHidden] = useState( false )
+
 
   return (
-    <div className='mx-auto'>
-      <div className='flex flex-row mx-auto w-min justify-center border px-7 rounded-full' onClick={(): void => setHidden( !hidden )}>
-        <h2>{t( 'filters' )}</h2>
-        <Bars3Icon className='h-8 inline pl-5 my-auto'/>
-      </div>
-      <div className={`mx-auto w-max ${hidden && 'hidden'}`}>
-        {tags.map( ( e ) => (
+    <Dropdown title='filters'>
+      <>
+        {allTags.map( ( e ) => (
           <TriStateSwitch
             key={e}
             text={e}
-            state={filters.tagStates[e]}
+            state={filters.tags[e]}
             setState={( newState: TriStateSwitchState ): void => {
-              updateFilters( { ...filters, tagStates: { ...filters.tagStates, [e]: newState } } )
+              if ( filters.tags[e] !== newState ) {
+                updateFilters( { ...filters, tags: { ...filters.tags, [e]: newState } } )
+              }
             }}
           /> )
         )}
-      </div>
 
-      <form className={`mx-auto flex flex-row flex-wrap justify-center ${hidden && 'hidden'}`}>
-        <div className='flex flex-col m-3'>
-          <p className='w-max'>{t( 'minDeepness' )}</p>
-          <select
-            defaultValue={filters.minDeepnessState}
-            onChange={( e ): void => updateFilters( { ...filters, minDeepnessState: Number.parseInt( e.target.value ) } )}
-          >
-            {possibleDeepnessLevels.map( e => ( <option key={e} disabled={e > filters.maxDeepnessState}>{e}</option> ) )}
-          </select>
-        </div>
-        <div className='flex flex-col m-3'>
-          <p className='w-max'>{t( 'maxDeepness' )}</p>
-          <select
-            defaultValue={filters.maxDeepnessState}
-            onChange={( e ): void => updateFilters( { ...filters, maxDeepnessState: Number.parseInt( e.target.value ) } )}
-          >
-            {possibleDeepnessLevels.map( e => ( <option key={e} disabled={e < filters.minDeepnessState}>{e}</option> ) )}
-          </select>
-        </div>
-      </form>
-    </div>
+        <form className={`mx-auto flex flex-row flex-wrap justify-center`}>
+          <div className='flex flex-col m-3'>
+            <p className='w-max'>{t( 'minDeepness' )}</p>
+            <select
+              value={filters.minDeepness}
+              onChange={( e ): void => updateFilters( { ...filters, minDeepness: Number.parseInt( e.target.value ) } )}
+            >
+              {possibleDeepnessLevels.map( e => ( <option key={e} disabled={e > filters.maxDeepness}>{e}</option> ) )}
+            </select>
+          </div>
+          <div className='flex flex-col m-3'>
+            <p className='w-max'>{t( 'maxDeepness' )}</p>
+            <select
+              value={filters.maxDeepness}
+              onChange={( e ): void => updateFilters( { ...filters, maxDeepness: Number.parseInt( e.target.value ) } )}
+            >
+              {possibleDeepnessLevels.map( e => ( <option key={e} disabled={e < filters.minDeepness}>{e}</option> ) )}
+            </select>
+          </div>
+        </form>
+      </>
+    </Dropdown>
   )
 }
 
@@ -97,9 +95,11 @@ const reduceFilters = ( filters: Array<( q: Question ) => boolean> ): ( q: Quest
   filters.reduce( ( acc, cur ): ( q: Question ) => boolean =>
     ( q ) => acc( q ) && cur( q ) )
 
+const filterIgnoredTags = ( [,v]: string[] ): boolean =>v !== 'IGNORE'
+
 const getFiltersForTags = ( tags: Record<string, TriStateSwitchState> ): Array<( q: Question ) => boolean> =>
   Object.entries( tags )
-    .filter( ( e ) => e[1] !== 'IGNORE' )
+    .filter( filterIgnoredTags )
     .map( ( [tag, state] ) => getFilterForTag( tag, state ) )
 
 
