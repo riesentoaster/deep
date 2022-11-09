df280fb980a96fe436f8e4e4f8cb68ae7b775f81
Valentin Huber <contact@valentinhuber.me>
2022-11-09 13:12:09 -0500

    whitelisting languages

--- pages/api/[lang].ts
+++ pages/api/[lang].ts
@@ -3,17 +3,22 @@ import fs from 'fs'
 import { apiify } from '../../helpers/apiify'
 import path from 'path'
 
-const locales = require( '../../next-i18next.config' ).i18n.locales
+const locales: string[] = require( '../../next-i18next.config' ).i18n.locales
 
 export default apiify(
   ( req, res ): void => {
-    if ( !req.query.lang ||
-      Array.isArray( req.query.lang ) ||
-      !locales.includes( req.query.lang ) ) {
-      res.status( 404 ).json( { message: 'Language not found.' } )
+    if ( !req.query.lang || Array.isArray( req.query.lang ) ) {
+      res.status( 404 ).json( { message: 'Did not provide language in appropriate format.' } )
+      return
     }
+    const offset = locales.indexOf( req.query.lang )
+
+    if ( offset < 0 ) {
+      res.status( 404 ).json( { message: 'Could not find language.' } )
+      return
+    }
+    const dir = path.resolve( './public',`locales/${locales[offset]}` )
     try {
-      const dir = path.resolve( './public',`locales/${req.query.lang}` )
       const translations = {
         questions: JSON.parse( '' + fs.readFileSync( path.join( dir, 'questions.json' ) ) ),
         tags: JSON.parse( '' + fs.readFileSync( path.join( dir, 'tags.json' ) ) )
