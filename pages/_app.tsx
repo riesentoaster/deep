692be930171358b9f47d2c9d62fe6da89795a980
Valentin Huber <git@valentinhuber.me>
2023-04-19 22:52:04 +0200

    adding vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp( { Component, pageProps }: AppProps ): JSX.Element {
   return (
-    <Component {...pageProps} />
-
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   )
 }
 
