623fa89a8a72921c8abb91ca12c1e6558b797750
Valentin Huber <git@valentinhuber.me>
2023-04-22 16:22:10 +0200

    fixing i18next on vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require( 'path' )
+
 module.exports = {
   // debug: process.env.NODE_ENV === 'development',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'en',
-  },
+    localePath: path.resolve( './public/locales' )
+  }
 }
