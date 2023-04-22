ecb6c88269324b2d83649c46d44a1e6475c27e68
Valentin Huber <git@valentinhuber.me>
2023-04-22 14:06:19 +0200

    making dropdown a button

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -13,12 +13,14 @@ export const Dropdown = ( { children, title, className='', defaultHiddenState=fa
   const [hidden, setHidden] = useState( defaultHiddenState )
   return (
     <div className={`mx-auto ${className}`}>
-      <Ellipsis className='mx-auto mb-2'>
-        <div className='flex flex-row' onClick={(): void => setHidden( !hidden )}>
-          <h2>{title}</h2>
-          <Bars3Icon className='h-6 inline pl-5 my-auto'/>
-        </div>
-      </Ellipsis>
+      <button className='mx-auto mb-2'>
+        <Ellipsis>
+          <div className='flex flex-row' onClick={(): void => setHidden( !hidden )}>
+            <h2>{title}</h2>
+            <Bars3Icon className='h-6 inline pl-5 my-auto'/>
+          </div>
+        </Ellipsis>
+      </button>
       <div className={`w-full ${hidden && 'hidden'}`}>
         {children}
       </div>
