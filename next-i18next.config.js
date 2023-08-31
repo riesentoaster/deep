0cb532b4413e3eee9605f1c76b6f63967436a944
Valentin Huber <git@valentinhuber.me>
2023-08-31 15:35:33 +0200

    fixing unnecessary i18next config entry

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,8 @@
-const path = require( 'path' )
-
 module.exports = {
   // debug: process.env.NODE_ENV === 'development',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'en',
-    localePath: path.resolve( './public/locales' )
   }
 }
