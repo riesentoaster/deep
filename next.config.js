0acbc9f3e08faed9c252934b5131d0ad26b878be
Valentin Huber <git@valentinhuber.me>
2023-08-31 15:35:08 +0200

    introducing offline caching

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const { i18n } = require( './next-i18next.config' )
+const wwp = require( 'workbox-webpack-plugin' )
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+  webpack: config => {
+    config.plugins.push( new wwp.GenerateSW( {
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 1e9,
+      swDest: '../public/service-worker.js',
+      ignoreURLParametersMatching: [/.*/],
+    } ) )
+    return config
+  },
 }
-
 module.exports = nextConfig
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { Workbox } from 'workbox-window'
 import { Analytics } from '@vercel/analytics/react'
+import { useEffect } from 'react'
 
 function MyApp( { Component, pageProps }: AppProps ): JSX.Element {
+
+  useEffect( () => {
+    if ( 'serviceWorker' in navigator )
+      new Workbox( 'service-worker.js' ).register()
+  }, [] )
+
   return (
     <>
       <Component {...pageProps} />
