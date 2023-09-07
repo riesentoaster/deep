9c47e37e1a0e04f035ca18ff6d9827cab59abc64
Valentin Huber <git@valentinhuber.me>
2023-09-07 11:53:46 +0200

    introducing translation-check

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,5 +1,7 @@
+import i18next from 'i18next'
 import { Ellipsis } from '../generic/Ellipsis'
 import { LanguageSettings } from './LanguageSettings'
+import { showTranslations } from 'translation-check'
 
 export const Footer = (): JSX.Element => (
   <footer className='flex flex-row justify-space flex-wrap justify-center p-5 gap-5'>
@@ -7,5 +9,7 @@ export const Footer = (): JSX.Element => (
     <Ellipsis>
       <p>Visit this project on <a href='https://github.com/riesentoaster/deep'>GitHub</a></p>
     </Ellipsis>
+    {process.env.NODE_ENV !== 'production' &&
+     <Ellipsis><button onClick={(): void => showTranslations( i18next ) }>Show Translations</button></Ellipsis> }
   </footer>
 )
