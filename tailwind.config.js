ae49ec4fccff99037ca2229ba1cd0ba95c5ca2d8
Valentin Huber <git@valentinhuber.me>
2023-09-30 18:10:39 +0200

    fixing spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './src/**/*.{js,ts,jsx,tsx}'],
+  content: [ './src/**/*.{js,ts,jsx,tsx}' ],
   theme: {
     extend: {},
   },
