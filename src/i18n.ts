1f442539865d7d68f56e391fb3f6c17295cf6f70
Valentin Huber <git@valentinhuber.me>
2023-10-18 22:21:10 +0200

    changing html lang attribute based on actual language

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,6 +6,8 @@ import { DEFAULT_LANGUAGE, questionTranslations } from './questions/language'
 import deCommon from './locales/de.json'
 import { tagTranslations } from './questions/tags'
 
+i18n.on( 'languageChanged', ( lng ) => ( document.documentElement.lang = lng ) )
+
 i18n
   .use( LanguageDetector )
   .use( initReactI18next ) // passes i18n down to react-i18next
@@ -25,3 +27,4 @@ i18n
     fallbackLng: DEFAULT_LANGUAGE,
     interpolation: { escapeValue: false }
   } )
+
