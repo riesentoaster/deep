f2c1421c78f61e70f5dd14403e7c5d30bb3c78ec
Valentin Huber <git@valentinhuber.me>
2024-09-27 16:33:30 +0200

    integrating web manifests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import eslint from 'vite-plugin-eslint'
 
+const viteConfig: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    background_color: '#010C1E',
+    theme_color: '#010C1E',
+    display: 'fullscreen',
+    description: 'Getting people to talk about the stuff that really matters',
+    icons: [
+      {
+        src: 'favicon.svg',
+        type: 'image/svg+xml',
+        purpose: 'any maskable',
+        sizes: '200x200'
+      },
+      {
+        src: 'google-touch-icon.png',
+        type: 'image/png',
+        sizes: '512x512'
+      }
+    ],
+  }
+}
+
 export default defineConfig( {
-  plugins: [ react(), eslint(), VitePWA( { registerType: 'autoUpdate' } ) ],
+  plugins: [ react(), eslint(), VitePWA( viteConfig ) ],
   define: { __BUILD_DATE__: `"${ new Date().toISOString() }"` },
   build: { chunkSizeWarningLimit: 10000 } // 10 MB
 } )
