86b17b56e7fc6486aad5d3cb95e1bca19e0ab03c
Valentin Huber <git@valentinhuber.me>
2025-03-24 22:44:53 +0100

    Housekeeping

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [ './index.html', './src/**/*.{js,ts,jsx,tsx}' ],
   theme: {
     extend: {},
